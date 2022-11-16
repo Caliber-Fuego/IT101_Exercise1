86a5a72d7af976ca22cd1130db065d2eb58939a4
Caliber-Fuego <louisfuego03@gmail.com>
2022-11-16 09:47:13 +0800

    test

---
+++ sample1.py
@@ -0,0 +1,12 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+first = int(input("Enter your first number: "))
+second = int(input("Enter your second number: "))
+
+print("Username:",name ,
+      "\nAge:", age,
+      "\nFirst number:", first,
+      "\nSecond number:", second,
+      "\nThe username is", name ,
+      "and the age is", age,
+      "\nThe product is", first + second)
